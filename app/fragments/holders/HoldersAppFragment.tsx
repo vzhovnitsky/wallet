932b2fd6eca3fdbc3d64a3a913d1c16c22a9b715
vzhovnitsky <vzhovnitsky@gmail.com>
2024-03-21 13:31:51 +0200

    cleanup

--- app/fragments/holders/HoldersAppFragment.tsx
+++ app/fragments/holders/HoldersAppFragment.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { fragment } from '../../fragment';
-import { Alert, View } from 'react-native';
+import { View } from 'react-native';
 import { HoldersAppComponent } from './components/HoldersAppComponent';
 import { useParams } from '../../utils/useParams';
 import { t } from '../../i18n/t';
-import { useEffect, useMemo } from 'react';
-import { useTypedNavigation } from '../../utils/useTypedNavigation';
-import { useHoldersAccountStatus, useHoldersIsReady, useNetwork, useSelectedAccount, useTheme } from '../../engine/hooks';
-import { HoldersAccountState, holdersUrl } from '../../engine/api/holders/fetchAccountState';
+import { useEffect } from 'react';
+import { useHoldersAccountStatus, useNetwork, useSelectedAccount, useTheme } from '../../engine/hooks';
+import { holdersUrl } from '../../engine/api/holders/fetchAccountState';
 import { StatusBar, setStatusBarStyle } from 'expo-status-bar';
 import { onHoldersInvalidate } from '../../engine/effects/onHoldersInvalidate';
 import { useFocusEffect } from '@react-navigation/native';
@@ -19,57 +18,7 @@ export const HoldersAppFragment = fragment(() => {
     const { isTestnet } = useNetwork();
     const params = useParams<HoldersAppParams>();
     const selected = useSelectedAccount();
-    const navigation = useTypedNavigation();
     const status = useHoldersAccountStatus(selected!.address).data;
-    const isHoldersReady = useHoldersIsReady(holdersUrl);
-
-    const needsEnrollment = useMemo(() => {
-        if (!isHoldersReady) {
-            return true;
-        }
-
-        if (!status) {
-            return true;
-        }
-
-        if (status.state === HoldersAccountState.NeedEnrollment) {
-            return true;
-        }
-
-        return false;
-    }, [status, isHoldersReady]);
-
-    useEffect(() => {
-        (async () => {
-            await new Promise<boolean>(resolve => {
-                Alert.alert(
-                    'HoldersApp',
-                    JSON.stringify({ needsEnrollment, status: status?.state, isHoldersReady }),
-                    [{
-                        text: t('common.ok'),
-                        onPress: () => {
-                            resolve(true)
-                        }
-                    }])
-            });
-
-            await new Promise<boolean>(resolve => {
-                Alert.alert(
-                    'HoldersApp',
-                    'Going back to previous screen...',
-                    [{
-                        text: t('common.ok'),
-                        onPress: () => {
-                            resolve(true)
-                        }
-                    }])
-            });
-
-            if (needsEnrollment) {
-                navigation.goBack();
-            }
-        })();
-    }, [needsEnrollment]);
 
     useEffect(() => {
         return () => {
@@ -88,14 +37,12 @@ export const HoldersAppFragment = fragment(() => {
             backgroundColor: theme.backgroundPrimary
         }}>
             <StatusBar style={theme.style === 'dark' ? 'light' : 'dark'} />
-            {needsEnrollment ? null : (
-                <HoldersAppComponent
-                    title={t('products.holders.title')}
-                    variant={params}
-                    token={(status as { token: string }).token}
-                    endpoint={holdersUrl}
-                />
-            )}
+            <HoldersAppComponent
+                title={t('products.holders.title')}
+                variant={params}
+                token={(status as { token: string }).token}
+                endpoint={holdersUrl}
+            />
         </View>
     );
 });
