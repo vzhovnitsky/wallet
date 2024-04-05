b422e2dbb8aa2dd33d5cac0a3e967d4ac87deecb
vzhovnitsky <vzhovnitsky@gmail.com>
2024-04-05 16:29:45 +0300

    Merge branch 'ref/hub-1014' into v2.0.0-status-bar
    # Conflicts:
    #	app/components/products/LedgerProductsComponent.tsx

--- app/components/products/LedgerProductsComponent.tsx
+++ app/components/products/LedgerProductsComponent.tsx
@@ -1,15 +1,13 @@
 import React, { } from "react"
 import { Text, View } from "react-native"
 import { t } from "../../i18n/t";
+import { StakingProductComponent } from "./StakingProductComponent";
 import { LedgerJettonsProductComponent } from "./LedgerJettonsProductComponent";
 import { useTheme } from "../../engine/hooks";
 import { Typography } from "../styles";
 import { AccountLite } from '../../engine/hooks/accounts/useAccountLite';
 import { useTypedNavigation } from '../../utils/useTypedNavigation';
-import { StakingProductComponent } from "./StakingProductComponent";
 import { TonProductComponent } from "./TonProductComponent";
-import { USDTProduct } from "./USDTProduct";
-import { Address } from "@ton/core";
 
 export const LedgerProductsComponent = React.memo(({ account, testOnly }: { account: AccountLite, testOnly: boolean }) => {
     const theme = useTheme();
@@ -40,23 +38,11 @@ export const LedgerProductsComponent = React.memo(({ account, testOnly }: { acco
                     isLedger
                 />
 
-                <USDTProduct
-                    key={'usdt-native'}
-                    theme={theme}
-                    navigation={navigation}
-                    isLedger
-                    address={Address.parse(account.address)}
-                    testOnly={testOnly}
-                />
-
                 <View style={{ marginTop: 4 }}>
                     <StakingProductComponent isLedger key={'pool'} />
                 </View>
-
                 <View style={{ paddingHorizontal: 16 }}>
-                    <View style={{ marginTop: 16 }}>
-                        <LedgerJettonsProductComponent key={'jettons'} />
-                    </View>
+                    <LedgerJettonsProductComponent key={'jettons'} />
                 </View>
             </View>
         </View>
